855d188568d187eadb0b37d390d2ded180098158
francisbitontistudio <francis@lexset.ai>
2018-11-14 17:41:40 -0500

    library start

--- genysis.py
+++ genysis.py
@@ -1,8 +1,24 @@
 import requests
+import json
 
-def marchingCubes(lines,resolution,memberThickness,filename,t):
+def stochasticLattice(volume,poreSize,filename,t):
 
-    url = ('https://studiobitonti.appspot.com/marchingCube?lines=%s&resolution=%s&memberThickness=%s&filename=%s&t=%s&wait=true'% (lines,resolution,memberThickness,filename,t))
-    ret = requests.get(url)
-    print(ret.text)
-    return url
+    url = "https://studiobitonti.appspot.com/stochasticLattice"
+    payload = {"volume":volume,"poreSize":poreSize,"filename":filename,"t":t}
+    r = requests.post(url,payload)
+    return r.text
+
+def volumeLattice(component,volume,componentSize,filename,t):
+
+    url = "https://studiobitonti.appspot.com/volumeLattice"
+    payload = {"component":component,"volume":volume,"componentSize":componentSize,"filename":filename,"t":t}
+    r = requests.post(url,payload)
+    return r.text
+
+def volumeLattice_attractor(component,volume,componentSize,filename,t):
+
+    url = "https://studiobitonti.appspot.com/volumeLattice"
+    payload = {"component":component,"volume":volume,"componentSize":componentSize,"filename":filename,"t":t,"blendTargets":[{"component":"unit_0.obj","attractor":{"point":[0,0,0],"range":2}}]}
+    print(json.dumps(payload, indent=4))
+    r = requests.post(url,payload)
+    return r.text
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import genysis
+import json
 
-x=genysis.marchingCubes("Meshing_example.obj",150,0.2,"Marching_Cube_Example",4)
+#x=genysis.stochasticLattice("S_lattice.obj",0.5,"S_lattice_Result.obj","DEV")
+
+#x= genysis.volumeLattice("Example_LatticeUnit.obj","Vol_Latt_Base.obj",1.2,"vol_Lattice_Example.obj","DEV")
+
+x = genysis.volumeLattice_attractor("unit_1.obj","Vol.obj",0.5,"vol_exmple_2.obj","DEV")
 print(x)
