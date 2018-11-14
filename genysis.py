fb42f5db1e4b35f49faeadc7d1bf952689d56a25
Li Chen <leochen1990@gmail.com>
2018-11-14 18:04:31 -0500

    post json bug fix

--- genysis.py
+++ genysis.py
@@ -5,20 +5,19 @@ def stochasticLattice(volume,poreSize,filename,t):
 
     url = "https://studiobitonti.appspot.com/stochasticLattice"
     payload = {"volume":volume,"poreSize":poreSize,"filename":filename,"t":t}
-    r = requests.post(url,payload)
+    r = requests.post(url,json=payload)
     return r.text
 
 def volumeLattice(component,volume,componentSize,filename,t):
 
     url = "https://studiobitonti.appspot.com/volumeLattice"
     payload = {"component":component,"volume":volume,"componentSize":componentSize,"filename":filename,"t":t}
-    r = requests.post(url,payload)
+    r = requests.post(url,json=payload)
     return r.text
 
 def volumeLattice_attractor(component,volume,componentSize,filename,t):
 
     url = "https://studiobitonti.appspot.com/volumeLattice"
     payload = {"component":component,"volume":volume,"componentSize":componentSize,"filename":filename,"t":t,"blendTargets":[{"component":"unit_0.obj","attractor":{"point":[0,0,0],"range":2}}]}
-    print(json.dumps(payload, indent=4))
-    r = requests.post(url,payload)
+    r = requests.post(url,json=payload)
     return r.text
