9db49ca286633a8d9d7d518e82add5dd0ba1881a
francisbitonti <francis@studiobitonti.com>
2018-12-06 16:54:34 -0500

    package documention

---
+++ python_package/genysis_pkg/docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["master"],envversion:52,filenames:["master.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to genysisDocs's documentation!"],titleterms:{document:0,genysisdoc:0,indic:0,tabl:0,welcom:0}})
