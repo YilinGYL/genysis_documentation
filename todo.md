03d162da8ab4a6a75f1c9dd929c46075fb26d937
francisbitonti <francis@studiobitonti.com>
2018-12-05 22:33:06 -0500

    cranial_implant tutorial updated and todo updated

--- todo.md
+++ todo.md
@@ -1,3 +1,9 @@
 add a warning if surfaces aren't quads
 test boolean operation (Francis)
 debug why zero values throw an error on the generate lattice function
+
+Something is wrong with the component generation function
+Following error generated.
+
+{"component":"cell_1.obj","base":"Skull_Srf.obj","cellHeight":0.1,"filename": "implant_2.obj","t":"dev","blendTargets":[{"component":"cell_2.obj","attractor":{"point":[1.6, 90.4, 0.6],"range":2}}]}
+<!DOCTYPE html><html><head><title></title><link rel="stylesheet" href="/stylesheets/style.css"></head><body><h2>Error code 500</h2><pre>components vertices number does not match with each other</pre><pre></pre></body></html>
--- tutorials/cranial_implant/main.py
+++ tutorials/cranial_implant/main.py
@@ -11,29 +11,45 @@ cellName = "cell_1.obj"
 #create a lattice unit
 #genLatticeUnit(case,chamfer,centerChamfer,bendIn,cBendIn,connectPt,output,token):
 #bug:function is currently not accepting 0.0 as a value
-genysis.genLatticeUnit(7,0.5,0.01,1.0,0.01,0.01,cellName,token)
+genysis.genLatticeUnit(7,0.5,0.01,1.0,0.01,1,cellName,token)
 
 #create a surface lattice class
 cranial =genysis.surfaceLattice()
+
+#set the values needed for this function
 cranial.setSurface("Skull_Srf.obj")
 cranial.setCellHeight(0.1)
 cranial.setOutput(MPfilename)
 cranial.setComponent(cellName)
+
+#Generate the lattice. It will be saved as implant.obj
 cranial.surfaceLatticeStatic(token)
 
+#the function will return a list of STL filesself.
+#high density meshes are computed distriubted for speed.
 final=genysis.marchingCube(MPfilename,900,0.01,"implantOutput",token)
 print(final)
 
 #Now lets generate one with a gradient
+#First set up a new lattice component to blend into
 newBlendTarget = "cell_2.obj"
-genysis.genLatticeUnit(7,0.01,0.01,0.01,0.01,0.01,newBlendTarget,token)
-attractorCenter = [1.4,89.89,0.5]
-radius = 4
+genysis.genLatticeUnit(7,0.01,0.01,0.01,0.01,1,newBlendTarget,token)
+
+#set up variables for the
+attractorCenter = [1.6,90.4,0.6]
+radius = 2
 attractor=[[newBlendTarget,attractorCenter,radius]]
 
+#set the values needed for this function
 cranial.setAttractor(attractor)
 cranial.setOutput("implant_2.obj")
+
+#generate new lattive with attractor.
 cranial.oneSurfaceLatticeAttractors(token)
 
+#the function will return a list of STL filesself.
+#high density meshes are computed distriubted for speed.
+#for example...
+#["implantOutputV2_5.stl","implantOutputV2_4.stl","implantOutputV2_3.stl","implantOutputV2_0.stl","implantOutputV2_1.stl","implantOutputV2_2.stl"]
 final=genysis.marchingCube("implant_2.obj",900,0.01,"implantOutputV2",token)
 print(final)
