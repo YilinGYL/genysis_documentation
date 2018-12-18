2b2e0628d0aba44501e044fbc8f9959e290bdc44
francisbitonti <francis@studiobitonti.com>
2018-12-18 17:19:21 -0500

    pydoc

--- python_package/genysis_pkg/genysis/__init__.py
+++ python_package/genysis_pkg/genysis/__init__.py
@@ -5,4 +5,4 @@ GENYSIS is a generitive design toolkit for software developers.
 
 from .genysis import *
 __version__ = '0.2.0'
-__all__ = ['volumeLattice','conformalVolume','surfaceLattice','cylindricalProjection','sphericalProjection','planarProjection','boolean','convexHull','voronoi','delaunay','blend','meshSplit','meshReduce','genLatticeUnit','marchingCube','download','upload']  # visible names
+__all__ = ['volumeLattice','conformalLattice','surfaceLattice','cylindricalProjection','sphericalProjection','planarProjection','boolean','convexHull','voronoi','delaunay','blend','meshSplit','meshReduce','genLatticeUnit','marchingCube','download','upload']  # visible names
